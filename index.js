7db1c836bb5733cb02335828c0803f1261cc7471
Joenell Canlas Alonzo <72643848+alonzojoe@users.noreply.github.com>
2023-08-11 14:28:02 +0800

    Modify the CORS headers of the http.createServer

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const server = http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello, WebSocket Server!');
 });
 const io = socketIo(server);
 
